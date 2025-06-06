8cdb691a8535e794c44648f4d8372aae82fae085
aivandag <aivan.dev7@gmail.com>
2025-06-06 23:22:36 +0000

    Table name user

--- src/models.py
+++ src/models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
+    __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
     lastname: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(
