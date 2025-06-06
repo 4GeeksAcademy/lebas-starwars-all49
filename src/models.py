7033d477d92abe8377f955c5e26d19bed45a2d4b
Deimian Vásquez <deimianvasquez@gmail.com>
2025-06-06 22:56:46 +0000

    instrucciones

--- src/models.py
+++ src/models.py
@@ -4,12 +4,13 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
@@ -17,3 +18,55 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+"""
+📝 Instrucciones
+
+Table user: 
+    id: int
+    lastname: str
+    email: str
+    password: str
+
+Table planet:
+    id: int
+    name: str
+    description: str
+
+Table people:
+    id: int
+    name: str
+    description: str
+
+    
+asociación 
+Table favorite:
+    id: int
+    user_id: int
+    planet_id: int     id ---  user_id ----- people_id ---- planet_id ----
+    people_id: int      1         1             1              null
+                        2         1             null             1       
+
+
+Crea una API conectada a una base de datos e implemente los siguientes endpoints (muy similares a SWAPI.dev or SWAPI.tech):
+
+    [GET] /people Listar todos los registros de people en la base de datos.
+    [GET] /people/<int:people_id> Muestra la información de un solo personaje según su id.
+    [GET] /planets Listar todos los registros de planets en la base de datos.
+    [GET] /planets/<int:planet_id> Muestra la información de un solo planeta según su id.
+
+Adicionalmente, necesitamos crear los siguientes endpoints para que podamos tener usuarios y favoritos en nuestro blog:
+
+    [GET] /users Listar todos los usuarios del blog.
+    [GET] /users/favorites Listar todos los favoritos que pertenecen al usuario actual.
+    [POST] /favorite/planet/<int:planet_id> Añade un nuevo planet favorito al usuario actual con el id = planet_id.
+    [POST] /favorite/people/<int:people_id> Añade un nuevo people favorito al usuario actual con el id = people_id.
+    [DELETE] /favorite/planet/<int:planet_id> Elimina un planet favorito con el id = planet_id.
+    [DELETE] /favorite/people/<int:people_id> Elimina un people favorito con el id = people_id.
+    [GET] /people-population Popula la tabla de base de datos de people --> Consultar apis desde el backend
+
+    Tu API actual no tiene un sistema de autenticación (todavía), es por eso que la única forma de crear usuarios es directamente en la base de datos usando el Flask admin.
+
+
+"""
